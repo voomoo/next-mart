6b812ccc52ef004f4f52cf26ac820811383fdd4a
unknown <rakib.7896@gmail.com>
2022-03-14 13:55:16 +0600

    work at login

--- pages/api/v1/auth/login.ts
+++ pages/api/v1/auth/login.ts
@@ -1,41 +1,64 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { ResponseData } from "../../../../dataTypes/types";
 import excuteQuery from "../../../../lib/db";
 import crypto from "crypto";
-import {v4} from "uuid"
-
-type Data = {
-  name: string;
-};
+import { v4 } from "uuid";
+import jwt from "jsonwebtoken";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ResponseData>
 ) {
   if (req.method === "POST") {
-    const { email, password, name } = req.body;
-    const salt = crypto.randomBytes(16).toString("hex");
-    const hash = crypto
-      .pbkdf2Sync(password, salt, 1000, 64, "sha512")
-      .toString("hex");
-      const user = {
-        id: v4(),
-        email,
-        hash,
-        name,
-        salt
-    };
-    console.log({user})
+    //receiving data from the body
+    const { email, password } = req.body;
+
+    //checking if required data is available
+    if (!(email && password)) {
+      return res
+        .status(422)
+        .json({ success: false, data: "Email & Password is required" });
+    }
+
     try {
-      const result: any = await excuteQuery({
-        query: "INSERT INTO users (id, email, password, name, salt) VALUES(?, ?, ?, ?, ?)",
-        values: [user.id, user.email, user.hash, user.name, user.salt],
+      //searching for the user
+      const user: any = await excuteQuery({
+        query: "SELECT * FROM users WHERE email = ?",
+        values: [email],
       });
-      console.log({ result: result });
+
+      console.log(typeof user[0].salt);
+
+      const userExists = user[0].length !== 0;
+
+      if (userExists) {
+        //hasing the password
+        const hash = crypto
+          .pbkdf2Sync(password, user[0].salt, 1000, 64, "sha512")
+          .toString("hex");
+        if (hash === user[0].password) {
+          const token = jwt.sign(
+            { id: user[0].id, email },
+            process.env.TOKEN_KEY as string,
+            {
+              expiresIn: "2h",
+            }
+          );
+
+          await excuteQuery({
+            query: "UPDATE users SET token = ? WHERE email = ?",
+            values: [token, email],
+          });
+          res.status(201).json({ success: true, data: { token: token } });
+        } else {
+          res.status(404).json({ success: false, data: "User not found" });
+        }
+      } else {
+        res.status(409).json({ success: false, data: "Email does not Exists" });
+      }
     } catch (error) {
       console.log({ error });
     }
   }
-
-  
 }
