6dd5bafd54462c70108553a5e9747ee6a15030c6
unknown <rakib.7896@gmail.com>
2022-03-14 18:04:53 +0600

    next work on Landing Page, Navbar, Sidebar and other UI

--- pages/auth/user-registration.tsx
+++ pages/auth/user-registration.tsx
@@ -1,14 +1,41 @@
-import type { NextPage } from "next"
+import type { NextPage } from "next";
+import { Button, Checkbox, Container, Form } from "semantic-ui-react";
 
-const UserRegistrationPage:NextPage = () => {
-    return(
-        <div>
-            <input type="text" placeholder="name" />
-            <input type="text" placeholder="email" />
-            <input type="text" placeholder="password" />
-            <input type="submit" value="submit" />
-        </div>
-    )
-}
+const UserRegistrationPage: NextPage = () => {
+  return (
+    <Container>
+      <Form>
+      <Form.Field>
+          <label>Name</label>
+          <input placeholder="Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Email</label>
+          <input placeholder="Email" />
+        </Form.Field>
+        <Form.Field>
+          <label>Phone</label>
+          <input placeholder="Phone" />
+        </Form.Field>
+        <Form.Field>
+          <label>Address</label>
+          <input placeholder="Address" />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+        </Form.Field>
+        <Form.Field>
+          <label>Confirm Password</label>
+          <input type="password" placeholder="Confirm Password" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default UserRegistrationPage;
